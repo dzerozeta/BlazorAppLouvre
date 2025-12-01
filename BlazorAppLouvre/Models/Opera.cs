e47c6c59066abad3bbda7c368beb1c1e9be4895e
dzedd <dzedd@MATED>
2025-12-01 07:39:45 +0100

    Aggiungere i file di progetto.

---
+++ BlazorAppLouvre/Models/Opera.cs
@@ -0,0 +1,49 @@
+﻿
+
+namespace BlazorAppLouvre.Models
+{
+    public class Opera
+    {
+        int idOpera;
+        string titolo;
+        string autore;
+        int annoCompletamento;
+        static int contaOpere = 1;
+        string imagefile = "";
+
+        public  Opera (string Titolo, string Autore, int AnnoCompletamento, string ImageFile="")
+        {
+            titolo = Titolo;
+            autore = Autore;
+            annoCompletamento = AnnoCompletamento;
+            idOpera = contaOpere;
+            contaOpere++;
+        }
+
+        public string TitoloEAutore
+        {
+            get { return titolo + "di" + autore; }
+        }
+
+        public int AnnoCompletamento
+        {
+            get { return annoCompletamento; }
+        }
+
+        public int ContaOpere
+        {
+            get { return contaOpere; }
+        }
+
+
+        public string OttieniInformazione()
+        {
+            return "L'opera è" + titolo + "di" + autore;
+        }
+
+        public string OttieniInformazioneDettagliata ()
+        {
+            return "L'opera è" + titolo + "di" + autore + "completata nel" + annoCompletamento;
+        }
+    }
+}
---
+++ BlazorAppLouvre/Models/Scultura.cs
@@ -0,0 +1,33 @@
+﻿namespace BlazorAppLouvre.Models
+{
+    public class Scultura : Opera
+    {
+        string materiale;
+        string tecnica;
+
+        public Scultura(string Titolo, string Autore, int AnnoCompletamento, string Materiale, string Tecnica, string ImageFile) : base (Titolo, Autore, AnnoCompletamento,ImageFile)
+        {
+            materiale = Materiale;
+            tecnica = Tecnica;
+        }
+
+        public string Materiale
+        { 
+             get { return materiale; }
+        }
+        public string Tecnica
+        {
+            get { return tecnica; }
+        }
+
+        public string OttieniInformazione ()
+        {
+            return base.OttieniInformazione() + " è una scultura" ;
+        }
+
+        public  string OttieniInformazioneDettagliata()
+        {
+            return base.OttieniInformazioneDettagliata + " è una scultura fatta in" + materiale + "usando la tecnica" + tecnica;
+        }
+    }
+}
